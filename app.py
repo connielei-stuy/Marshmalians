f8386408445cf8a425a20143d00ed1f2ec23e4af
Max Z <max.zlotskiy@gmail.com>
2018-04-11 23:32:42 -0400

    renders home page and it serves the js file correctly

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, redirect, url_for, request
+import utils.trees as trees
+
+app = Flask(__name__)
+
+#home page?
+@app.route('/')
+def home():
+    return render_template("map.html")
+
+#This will be requested by map.html
+@app.route('/treedots.js')
+def jsfile():
+    return render_template("treedots.js", trees=trees.jsonString)
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
