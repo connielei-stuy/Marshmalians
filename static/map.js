957e273377eae8f147c3c9914632a1c794cef3b6
fahmed3 <fahmed3@stuy.edu>
2018-04-13 20:36:59 -0400

    trees plotted

--- static/map.js
+++ static/map.js
@@ -1,12 +1,15 @@
-var data = {"objects":[
+/*var data = {"objects":[
     {"circle":{"coordinates":[40.78,-73.97]}},
     {"circle":{"coordinates":[40.69,-73.96]}},
     {"circle":{"coordinates":[40.70,-73.99]}},
     {"circle":{"coordinates":[40.77,-73.90]}},
     {"circle":{"coordinates":[40.79,-73.98]}}
-    ]}
+    ]}*/
 
 /*var data = JSON.parse(document.getElementById('map').getAttribute("name"));*/
+
+var data = JSON.parse(d);
+
 
 var map = L.map('map').setView([40.707895, -73.931150], 11);
 mapLink = 
@@ -26,13 +29,13 @@ var svg = d3.select("#map").select("svg"),
 
 var dots = function(collection) {
 
-    collection.objects.forEach(function(d) {
-	d.LatLng = new L.LatLng(d.circle.coordinates[0],
-				d.circle.coordinates[1])
+    collection.forEach(function(d) {
+	d.LatLng = new L.LatLng(d.latitude,
+				d.longitude)
     })
     
     var feature = g.selectAll("circle")
-	.data(collection.objects)
+	.data(collection)
 	.enter().append("circle")
 	.style("stroke", "black")  
 	.style("opacity", .6) 
