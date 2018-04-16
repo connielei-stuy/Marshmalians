429fc1689474b493e39bd8adb9289ed51248dbe4
Max Z <max.zlotskiy@gmail.com>
2018-04-16 14:42:54 -0400

    changed the name of trees module

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 from flask import Flask, render_template, redirect, url_for, request
-import utils.trees as trees
+import utils.data as data
 
 app = Flask(__name__)
 
 #home page?
 @app.route('/')
 def home():
-    return render_template("map.html", trees=trees.jsonString)
+    return render_template("map.html", trees=data.jsonString)
 
 #This will be requested by map.html
 @app.route('/treedots.js')
 def jsfile():
-    return render_template("treedots.js", trees=trees.jsonString)
+    return render_template("treedots.js", trees=data.jsonString)
 
 if __name__ == "__main__":
     app.debug = True
