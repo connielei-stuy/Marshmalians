bcb0b7e0018389b280750b2b1cb449fa77d8f663
clei1 <clei1@stuy.edu>
2018-04-19 00:20:02 -0400

    i am so sorry lord jesus

--- templates/treedots.js
+++ templates/treedots.js
@@ -17,8 +17,9 @@ map._initPathRoot();
 var svg = d3.select("#map").select("svg"),
     g = svg.append("g");
 
-//var dis = document.getElementById("display");
+var info = document.getElementById("info");
 
+//---------- TREES FUNCTIONS -------------//
 var selecting = function(obj){
     obj.style("fill", "blue");
 }
@@ -27,7 +28,7 @@ var disselect = function(obj, health, htmldisplay){
     if(health == "Good") obj.style("fill","green");
     else if(health == "Poor") obj.style("fill", "yellow");
     else obj.style("fill", "orange");
-    //dis.removeChild(htmldisplay);
+    info.removeChild(htmldisplay);
 }
 
 var display = function(data){
@@ -36,50 +37,8 @@ var display = function(data){
     console.log(data);
     d.innerHTML = (`${data["address"]} ${data["boroname"]} ${data["zip_city"]} ${data["zipcode"]}`);
     console.log(d.innerHTML);
-    //dis.appendElement(d);
-    //return d;
-}
-
-var addFeature = function(g, collection){
-    var feature = g.selectAll("circle")
-	.data(collection)
-	.enter().append("circle");
-
-    feature.style("stroke", "black")  
-	.style("opacity", .6) 
-	.style("fill", function(d){
-	    if(d.health == "Good") return "green";
-	    else if(d.health == "Poor") return "yellow";
-	    else return "orange";
-	})
-	.attr("r", 10)
-	.on({
-            "mouseout": function() { let obj = d3.select(this);
-				     let data = obj.datum();
-				     disselect(obj, data["health"], null);
-				   },
-            "mouseover": function() { let obj = d3.select(this);
-				      let data = obj.datum();
-
-				      selecting(obj);
-				      //let htmldisplay = display(data);
-				      /*
-					obj.on("mouseout", function(){
-					let obj = d3.select(this);
-					let data = obj.datum();
-					//disselect(obj, data["health"], htmldisplay);
-					}
-					);
-				      */
-				    }, 
-            "click":  function() { let obj = d3.select(this);
-				   let data = obj.datum();
-
-				   selected(obj, data);
-				 } 
-        });
-    
-    return feature;
+    info.appendChild(d);
+    return d;
 }
 
 var selected = function(obj, data){
@@ -87,38 +46,81 @@ var selected = function(obj, data){
     let htmldisplay = display(data);
     obj.on({"mouseout": function(){},
 	    "click": function(){ disselect(obj, data["health"], htmldisplay);
-				 obj.on("click", function(){
-				     unselected(obj, data);
-				 });
-			       }
-	   });
+				 obj.on({"click": function(){ unselected(obj, data); },
+					 "mouseout": function(){ disselect(obj, data["health"], null); },
+					 "mouseover": function(){selecting(obj);
+								 let hd = display(data);
+								 obj.on({"mouseout": function(){ let obj = d3.select(this);
+												 let data = obj.datum();
+												 disselect(obj, data["health"], hd); },
+									 "click": function(){ disselect(obj, data["health"], hd);
+											      selected(obj, data); } });}
+					});
+			       },
+	    "mouseover": function(){} });
 }
 
 var unselected = function(obj, data){
     selected(obj, data);
 }
+//---------- TREES FUNCTIONS -------------//
 
-var dots = function(collection) {
+var addFeature = function(g, collection, type){
+    var feature = g.selectAll("circle")
+	.data(collection)
+	.enter().append("circle");
+
+    feature.style("stroke", "black")  
+	.style("opacity", .6)
+	.attr("r", 10);
+
+    if(type == "trees"){
+	feature.style("fill", function(d){
+	    if(d.health == "Good") return "green";
+	    else if(d.health == "Poor") return "yellow";
+	    else return "orange";
+	})
+	    .on({"mouseout": function() { let obj = d3.select(this);
+					  let data = obj.datum();
+					  disselect(obj, data["health"], null); },
+		 "mouseover": function() { let obj = d3.select(this);
+					   let data = obj.datum();
+					   selecting(obj);
+					   let htmldisplay = display(data);
+					   console.log(htmldisplay);
+					   obj.on({"mouseout": function(){ let obj = d3.select(this);
+									   let data = obj.datum();
+									   disselect(obj, data["health"], htmldisplay); },
+						   "click": function(){ disselect(obj, data["health"], htmldisplay);
+								        selected(obj, data); } }); }, 
+		 "click":  function() { let obj = d3.select(this);
+					let data = obj.datum();
+					selected(obj, data); } 
+		});
+    }
+
+    else if(type == "fire"){ }
+    else if (type == "crime"){ }
+    else if (type == "shelters"){ }
+    
+    return feature;
+}
+
+var dots = function(collection, type) {
 
     collection.forEach(function(d) {
 	d.LatLng = new L.LatLng(d.latitude,
 				d.longitude)
     })
     
-    var feature = addFeature(g, collection);
+    var feature = addFeature(g, collection, type);
 
     map.on("viewreset", update);
     update();
 
     function update() {
-	feature.attr("transform", 
-		     function(d) { 
-			 return "translate("+ 
-			     map.latLngToLayerPoint(d.LatLng).x +","+ 
-			     map.latLngToLayerPoint(d.LatLng).y +")";
-		     }
-		    )
+	feature.attr("transform", function(d) { return "translate("+ map.latLngToLayerPoint(d.LatLng).x +","+ map.latLngToLayerPoint(d.LatLng).y +")"; })
     }
 }
 
-dots(data_2015_trees);
+dots(data_2015_trees, "trees");
