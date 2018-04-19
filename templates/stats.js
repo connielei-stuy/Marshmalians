84c91fcdca33c73574bd770555ba0a4be4173fda
Max Z <max.zlotskiy@gmail.com>
2018-04-19 14:49:09 -0400

    fixed it to show a list of all species and their counts

--- templates/stats.js
+++ templates/stats.js
@@ -1,6 +1,8 @@
-var species = {{ species }};
+var species = {{ species | tojson }};
 var container = document.getElementById("piechart");
 
 var loadBorough = function(name) {
     
 }
+
+for(var k in species) container.innerHTML += (k + ": " + species[k] + "<br>");
