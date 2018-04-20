7c360dddde68fe4f19b14d0d6f05555ae403bf50
Max Z <max.zlotskiy@gmail.com>
2018-04-19 21:36:02 -0400

    fixed the colors on the pie chart

--- templates/stats.js
+++ templates/stats.js
@@ -1,8 +1,13 @@
 var species = {{ species | tojson }};
-//var container = document.getElementById("piechart");
+var numSpecies = 0, numTrees = 0;
+for(var k in species) {
+	numTrees += species[k]
+	numSpecies++;
+}
+var container = document.getElementById("speciesTable");
+for(var k in species) container.innerHTML += "<tr><td>" + k + "</td><td>" + species[k] + "</td></tr>";
 
-for(var k in species) document.body.innerHTML += (k + ": " + species[k] + "<br>");
-
+/* Converts three numbers (r, g, b) into a CSS hex color code */
 var rgbToHex = function(r, g, b) {
     var str = "#";
     r = Math.min(255, parseInt(r));
@@ -11,35 +16,44 @@ var rgbToHex = function(r, g, b) {
     var digit = function(n) {
 	return ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'][n];
     };
-    str += digit(r % 16);
     str += digit(parseInt(r / 16));
-    str += digit(g % 16);
+    str += digit(r % 16);
     str += digit(parseInt(g / 16));
-    str += digit(b % 16);
+    str += digit(g % 16);
     str += digit(parseInt(b / 16));
+    str += digit(b % 16);
     return str;
 }
 
+/* This function returns a hex color code that is mostly green for the pie chart */
 var getGreenColor = function(index) {
-    var g = Math.random() * 200 + 10 * index;
-    return rgbToHex((5 * (g % 20)), g, (5 * (g / 20)));
+    var g = 100 + 155 * (index / numSpecies);
+    var other = 100 + 50 * (index / (numSpecies + 10));
+    return rgbToHex(other, g, other);
 };
 
+/* This converts the json object into the format that d3 needs it in */
 var loadSpeciesData = function() {
     var content = [];
+    var values = [];
     for(var k in species) {
-	label = k;
-	if(k == "") label = "Unknown";
-	content.push({
-	    label: label,
-	    value: species[k],
-	    color: getGreenColor(content.length)
-	});
-	console.log(content[content.length - 1].color);
+		label = k;
+		if(k == "") label = "Unknown";
+		content.push({
+		    label: label,
+		    value: species[k],
+		    color: "#00FF00"
+		});
+		values.push(species[k]);
+    }
+    values.sort();
+    for(var i = 0; i < content.length; i++) {
+    	content[i].color = getGreenColor(values.indexOf(content[i].value));
     }
     return content;
 };
 
+/* This creates the pie chart */
 var pie = new d3pie("pieChart", {
 	"header": {
 		"title": {
